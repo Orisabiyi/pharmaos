69e9c82620a9966aa4051816b6e558649bb38855
orisabiyi <orisabiyidavid@gmail.com>
2026-09-07 12:36:29 +0100

    chore: generate top nav component

---
+++ src/app/top-nav/top-nav.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  imports: [],
+  selector: "app-top-nav",
+  styleUrl: "./top-nav.css",
+  templateUrl: "./top-nav.html",
+})
+export class TopNav {}
