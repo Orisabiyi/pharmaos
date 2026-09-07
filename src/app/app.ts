2c460ec894695e8ba60b782fb176da8512ef91c3
orisabiyi <orisabiyidavid@gmail.com>
2026-09-07 13:45:08 +0100

    @ng-icons/feather-icons

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './sidebar/sidebar';
+import { TopNav } from "./top-nav/top-nav";
 
 @Component({
-  imports: [RouterOutlet, Sidebar],
+  imports: [RouterOutlet, Sidebar, TopNav],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
--- src/app/top-nav/top-nav.ts
+++ src/app/top-nav/top-nav.ts
@@ -1,9 +1,12 @@
+import { DatePipe } from "@angular/common";
 import { Component } from "@angular/core";
 
 @Component({
-  imports: [],
+  imports: [DatePipe],
   selector: "app-top-nav",
   styleUrl: "./top-nav.css",
   templateUrl: "./top-nav.html",
 })
-export class TopNav {}
+export class TopNav {
+  dateValue: number = Date.now();
+}
